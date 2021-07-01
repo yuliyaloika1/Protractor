18295ae278325d13c4a051470203c84fe2435f06
Yuliya Loika <yuliya_loika@epam.com>
2021-07-01 16:33:19 +0300

    commit

--- pages/utils/page_objects/pageFactory.js
+++ pages/utils/page_objects/pageFactory.js
@@ -3,6 +3,7 @@ const InsightsPage = require("./insights_page/insights_page");
 const BasePage = require("./base_page/base_page");
 const CareersPage = require("./careers_page/careers_page");
 
+
 class PageFactory {
     static getPage(pageName) {
         switch (pageName) {
